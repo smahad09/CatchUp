655d952b56e3f8e716d565e81bac8045decc664c
smahd09 <mahadsyed8@gmail.com>
2022-05-25 10:31:00 -0400

    final

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -18,6 +18,7 @@ const Navbar = ()=> {
         window.location.reload();
     };
 
+    //just adding to make a commit 
     return (
         <div className='navbar'>
             <div className="navbarLeft">
